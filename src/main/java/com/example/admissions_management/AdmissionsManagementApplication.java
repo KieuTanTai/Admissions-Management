604d56e0ce35599fecb3ea55d862cb14e0bc2a9d
KieuTanTai <kieutantai2x@gmail.com>
2026-04-26 10:04:05 +0700

    fix conflict driver db, headless app

--- src/main/java/com/example/admissions_management/AdmissionsManagementApplication.java
+++ src/main/java/com/example/admissions_management/AdmissionsManagementApplication.java
@@ -2,12 +2,14 @@ package com.example.admissions_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AdmissionsManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdmissionsManagementApplication.class, args);
+		// SpringApplication.run(AdmissionsManagementApplication.class, args);
+		new SpringApplicationBuilder(AdmissionsManagementApplication.class).headless(false).run(args);
 	}
 
 }
